c357092f6395b7b76e917a823b50d2ddefb6d91b
Asad Hussain <asadh@utexas.edu>
2024-10-27 22:39:57 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='truncatedgaussianmixtures',
-    version='0.1.3',
+    version='0.1.7',
     description="Fit gaussian mixture models using truncated gaussian kernels. This is a python wrapper around the julia package TruncatedGaussianMixtures.jl",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',

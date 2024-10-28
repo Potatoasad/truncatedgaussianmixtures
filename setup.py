005a97e93ab1c85e48dc3c91a52b3c482e1a429f
Asad Hussain <asadh@utexas.edu>
2024-10-27 22:12:17 -0500

    Update setup.py - removed long description

--- setup.py
+++ setup.py
@@ -10,7 +10,6 @@ setup(
     name='truncatedgaussianmixtures',
     version='0.1.0',
     description='''Fit gaussian mixture models using truncated gaussian kernels. This is a python wrapper around the julia package TruncatedGaussianMixtures.jl''',
-    long_description=readme,
     author='Asad Hussain',
     author_email='asadh@utexas.edu',
     url='https://github.com/potatoasad/truncatedgaussianmixtures',
